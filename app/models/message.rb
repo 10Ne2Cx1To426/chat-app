4d127609d47fd689ed58409a3a5af8791c975f5c
Sena <NseCnaT@users.noreply.github.com>
2020-11-23 09:24:47 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,12 @@ class Message < ApplicationRecord
 
   belongs_to :room
   belongs_to :user
+
+  #Active Storageデーブルに画像を保存する実装を行う
+  #ここで用いるのがhas_one_attachedメソッド
+  #レコードとファイルを1対1の関係で紐付ける
+  #has_one_attachedメソッドを記述したモデルの各レコードは、それぞれ１つのファイルを添付できる
+  #ファイル名は、そのモデルが紐づいたフォームから送られるパラメーターのキーにもなる
+
+  has_one_attached :image
 end
